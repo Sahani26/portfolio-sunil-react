8e7ddff5ed5c8c1942842c8112c07ca2b9fdfbce
Sahani26 <sunilsahani484@gmail.com>
2025-08-30 22:16:00 +0530

    change main 2 branch for testing

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/Experience.css"; // Ensure you create a CSS file for styling
 
 const timelineData = [
-  { year: "2020", title: "Title 1", descr: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!", color: "#41516C" },
+  { year: "2020", tsdfdsfdsfdsfdsfdsfdsitle: "Title 1", descr: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!", color: "#41516C" },
   { year: "2007", title: "Title 2", descr: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos adipisci nobis nostrum vero nihil veniam.", color: "#FBCA3E" },
   { year: "2012", title: "Title 3", descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga minima consequuntur soluta placeat iure totam commodi repellendus ea delectus, libero fugit quod reprehenderit, sequi quo, et dolorum saepe nulla hic.", color: "#E24A68" },
   { year: "2017", title: "Title 4", descr: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit, cumque.", color: "#1B5F8C" },
